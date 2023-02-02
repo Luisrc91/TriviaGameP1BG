cd1ce63baee5b26d35da3a417e919eb7039cf5f5
Luis <luisrc91@live.com>
2023-02-01 19:27:38 -0500

    fixed choice buttons

--- index.js
+++ index.js
@@ -1,25 +1,88 @@
-
 const questionCont = document.getElementById('questionBox')
 const answersBtns = document.querySelectorAll('buttons')
+const nextBtn = document.getElementById('nextBtn')
 
+const questions = [
+    {
+        number : 0,    
+        questionPic: `<img src='./assets/animalPictures/Dog.jpg' alt="Dog">`,
+        choices: [
+            {answer: 'Dog', correct: true}, 
+            {answer: 'Cat', correct: false},
+            {answer: 'Horse', correct: false},
+            {answer:'Bird', correct: false},
+        ]
+    },
+    {
+        number : 1, 
+        questionPic: `<img src='./assets/animalPictures/Tiger.jpg' alt="Tiger">`,
+        choices: [
+            {answer: 'Lion', correct: false}, 
+            {answer: 'Tiger', correct: true},
+            {answer: 'Puma', correct: false},
+            {answer: 'Leopard', correct: false},
+        ]
+    },
+    {
+        number : 2, 
+        questionPic: `<img src='./assets/animalPictures/Rhino.jpg' alt="Rhino">`,
+        choices: [
+            {answer: 'Elephant', correct: false}, 
+            {answer: 'Turtle', correct: false},
+            {answer: 'Antilope', correct: false},
+            {answer: 'Rhino', correct: true},
+        ]
+    },
+    {
+        number : 3, 
+        questionPic:`<img src='./assets/animalPictures/Turkey.jpg' alt="Turkey">`,
+        choices: [
+            {answer: 'Chicken', correct: false}, 
+            {answer: 'Pig', correct: false},
+            {answer: 'Turkey', correct: true},
+            {answer: 'Cow', correct: false},
+        ]
+    },
+    {
+        number : 4, 
+        questionPic: `<img src='./assets/animalPictures/polarBear.jpg' alt="Polar Bear">`,
+        choices: [
+            {answer: 'Zebra', correct: false}, 
+            {answer: 'Polar Bear', correct: true},
+            {answer: 'Wolf', correct: false},
+            {answer: 'Fox', correct: false},
+        ]
+    }
+]
 
 //displaying question images//
 const questionImage = document.getElementById('questionImg')
 
-function displayQuestion(questionPic){
-    questionImage.innerHTML = questions[0].questionPic
+function displayQuestion(currentQuestion){
+    console.log(shuffledQuestions[currentQuestion])
+    questionImage.innerHTML = shuffledQuestions[currentQuestion].questionPic
+    const choices = shuffledQuestions[currentQuestion].choices
+    choices.forEach(choice => {
+        const button = document.createElement('button')
+        button.textContent = choice.answer
+        button.class = 'buttons'
+        button.id = choice.correct
 
-    
-       
-      }
+        button.addEventListener('click', (e) => {
+            if (e.target.id === true) {
+                // make score go up by one and update dom
+                currentQuestion++
+                nextQuestion(currentQuestion)
+            } else {
+                currentQuestion++
+                nextQuestion(currentQuestion)
+            }
+        })
 
-function displayChoices(questions){
-    const button = document.createElement('button')
-    button.innerHTML = choices['Dog']
-    answersBtns.innerText = questions.answers
+        const choiceList = document.getElementById('buttonContr')
+        choiceList.appendChild(button)
+    })
 }
-
-
 
 //shuffle questions
 let shuffledQuestions, currentQuestion
@@ -27,145 +90,28 @@ let shuffledQuestions, currentQuestion
 const playButton = document.getElementById('start')
 playButton.addEventListener('click', playGame)
 
-   function playGame(){
-    // console.log('gamestarted')
-    shuffledQuestions = questions.sort(()=> Math.random()- .5)
+shuffledQuestions = questions.sort(()=> Math.random()- .5)
+console.log(shuffledQuestions)
+
+function playGame(){
     currentQuestion = 0
-    // GOT DISAPEAR PLAY BUTTON FROM THIS SITE BELOW//
-    // https://stackoverflow.com/questions/8685107/hiding-a-button-in-javascript // // 
     if(playGame) {
         document.getElementById('start').style.visibility = 'hidden';
     } else {
         document.getElementById('start').style.visibility = 'visible';
     }
-    displayQuestion()
+    displayQuestion(currentQuestion)
 }
+
+document.getElementById('nextBtn').addEventListener('click', nextQuestion)
 
 //nextquestion
-function nextQuestion(){
-    displayQuestion(shuffledQuestions[currentQuestion])
+function nextQuestion() {
+    const node = document.getElementById('buttonContr')
+    while (node.firstChild) {
+        node.removeChild(node.lastChild);
+    }
+
+    currentQuestion++
+    displayQuestion(currentQuestion)
 }
-
-
-
-// function answerSelection (answers){
-//     answersBtns.innerText = questions.answers
-//     // questions.answers =
-// }
-
-// GOT AN IDEA ON THIS SITES BELOW //
-//https://stackoverflow.com/questions/40569293/displaying-questions-for-a-quiz-using-javascript//
-// https://stackoverflow.com/questions/49729105/how-to-add-an-image-to-javascript-quiz//
-const questions = [
-    {
-    number : 0,    
-    questionPic: `<img src='./assets/animalPictures/dog.jpg' alt="Dog">`,
-    choices: [
-        {answer: 'Dog', correct: true}, 
-        {answer: 'Cat', correct: false},
-        {answer: 'Horse', correct: false},
-        {answer:'Bird', correct: false},
-    ]
-    
-},
-
-    {
-    number : 1, 
-    questionPic: `<img src='./assets/animalPictures/tiger.jpg' alt="Tiger">`,
-    choices: [
-        {answer: 'Lion', correct: false}, 
-        {answer: 'Tiger', correct: true},
-        {answer: 'Puma', correct: false},
-        {answer: 'Leopard', correct: false},
-    ]
-},
-
-    {
-    number : 2, 
-    questionPic: `<img src='./assets/animalPictures/rhino.jpg' alt="Rhino">`,
-    choices: [
-        {answer: 'Elephant', correct: false}, 
-        {answer: 'Turtle', correct: false},
-        {answer: 'Antilope', correct: false},
-        {answer: 'Rhino', correct: true},
-    ]
-},
-  {
-    number : 3, 
-    questionPic:`<img src='./assets/animalPictures/turkey.jpg' alt="Turkey">`,
-    choices: [
-        {answer: 'Chicken', correct: false}, 
-        {answer: 'Pig', correct: false},
-        {answer: 'Turkey', correct: true},
-        {answer: 'Cow', correct: false},
-    ]
-},
-
-    {
-    number : 4, 
-    questionPic: `<img src='./assets/animalPictures/polarBear.jpg' alt="Polar Bear">`,
-    choices: [
-        {answer: 'Zebra', correct: false}, 
-        {answer: 'Polar Bear', correct: true},
-        {answer: 'Wolf', correct: false},
-        {answer: 'Fox', correct: false},
-    ]
-}
-]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getElementById(ids) {
-//     var idList = ids.split(" ");
-//     var results = [], item;
-//     for (var i = 0; i < idList.length; i++) {
-//         item = document.getElementById(idList[i]);
-//         if (item) {
-//             results.push(item);
-//         }
-//     }
-//     return(results);
-// }
-
-// document.addEventListener('Click', )(getElementsById("#answerOne, #answerTwo, #answerThree, #answerFour"));
-
-// hoover event
-
-// let anwerBox= document.getElementsByClassName('answer');
-
-// //  Change text color on mouseover
-// anwerBox.addEventListener('mouseover', function handleMouseOver() {
-//   anwerBox.style.backgroundColor = 'yellow';
-// });
-// //  Change text color back on mouseout
-// anwerBox.addEventListener('mouseout', function handleMouseOut() {
-//   anwerBox.style.backgroundColor = '#6ea2eb';
-// });
-
-
